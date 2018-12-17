433bdad40abdbfad82e85356b10c59c7ef41053e
tanzimelahi <telahi00@stuy.edu>
2018-12-16 23:59:29 -0500

    fdfdfd

---
+++ LinkedList.java
@@ -0,0 +1,50 @@
+
+public class Null{
+
+
+  private class Node{
+   private Integer data;
+   private Node next,prev;
+   public Node next(){
+     public Node(Integer data,Node next,Node prev){
+       this.data=data;
+       this.next=next;
+       this.prev=prev;
+
+     }
+     return Node next;
+   }
+   public Node prev(){
+     return Node prev;
+   }
+   public void setNext(Node other){
+     next=other;
+   }
+   public void setPrev(Node other){
+     other==other;
+   }
+   public Integer getData(){
+     return this.data;
+   }
+   public Integer setData(Integer i){
+     this.data=i;
+   }
+   public String toString(){
+     return Integer.toString(data);
+   }
+
+  }
+  public class MyLinkedList{
+    private Node start;
+    private Node end;
+    private int length;
+    public MyLinkedList(){
+      length=0;
+      start=null;
+      end=null;
+    }
+  }
+  public boolean add(Integer value){
+    
+  }
+}
